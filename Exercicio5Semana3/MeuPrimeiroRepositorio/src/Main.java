9ecc6a95e771f7645a4484fb63abee48c899bbf4
Marcel Ferreira Campos <marcelfcampos@gmail.com>
2024-02-14 11:13:31 -0300

    [M1S03] Ex 5 - Utilizando o GitBash/Terminal crei um projeto básico no IntelliJ e envie para sua conta no GitHub usando os comandos do Git
    [M1S03] Ex 5 - Utilizando o GitBash/Terminal crei um projeto básico no IntelliJ
    e envie para sua conta no GitHub usando os comandos do Git

---
+++ Exercicio5Semana3/MeuPrimeiroRepositorio/src/Main.java
@@ -0,0 +1,10 @@
+/*
+[M1S03] Ex 5 - Utilizando o GitBash/Terminal crei um projeto básico no IntelliJ
+e envie para sua conta no GitHub usando os comandos do Git
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
